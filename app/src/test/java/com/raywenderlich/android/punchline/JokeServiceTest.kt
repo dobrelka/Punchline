90cffee890995fd0ef9663bb11aa1bef213d506a
dobrelka <dobrelyatest@gmail.com>
2020-11-03 12:42:12 +0200

    Adding rule in JokeServiceTest.kt

--- app/src/test/java/com/raywenderlich/android/punchline/JokeServiceTest.kt
+++ app/src/test/java/com/raywenderlich/android/punchline/JokeServiceTest.kt
@@ -1,5 +1,11 @@
 package com.raywenderlich.android.punchline
 
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.Rule
+
 class JokeServiceTestUsingMockWebServer {
+
+  @get:Rule
+  val mockWebServer = MockWebServer()
 
 }
