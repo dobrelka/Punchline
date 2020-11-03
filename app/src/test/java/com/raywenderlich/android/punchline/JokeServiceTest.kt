c1b0759fde0797d7252042e9b5b0cc775c4759f6
dobrelka <dobrelyatest@gmail.com>
2020-11-03 14:15:11 +0200

    Adding val retrofit in JokeServiceTest.kt

--- app/src/test/java/com/raywenderlich/android/punchline/JokeServiceTest.kt
+++ app/src/test/java/com/raywenderlich/android/punchline/JokeServiceTest.kt
@@ -1,11 +1,22 @@
 package com.raywenderlich.android.punchline
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.Rule
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class JokeServiceTestUsingMockWebServer {
 
   @get:Rule
   val mockWebServer = MockWebServer()
+
+  private val retrofit by lazy {
+    Retrofit.Builder()
+        .baseUrl(mockWebServer.url("/"))
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+  }
 
 }
