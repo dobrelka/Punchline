41df51b8acadf8e9143fa39f7d50394c6b07a562
dobrelka <dobrelyatest@gmail.com>
2020-11-04 15:50:12 +0200

    Adding val id and val joke in JokeService.kt

--- app/src/test/java/com/raywenderlich/android/punchline/JokeServiceTest.kt
+++ app/src/test/java/com/raywenderlich/android/punchline/JokeServiceTest.kt
@@ -8,7 +8,9 @@ import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private val testJson = """{ "id": 1, "joke": "joke" }"""
+private const val id = 6
+private const val joke = "How does a train eat? It goes chew, chew"
+private val testJson = """{ "id": $id, "joke": "$joke" }"""
 
 class JokeServiceTestUsingMockWebServer {
 
