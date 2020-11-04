69e1ced75ecbd558664ce383d362569e118ce03c
dobrelka <dobrelyatest@gmail.com>
2020-11-04 15:19:57 +0200

    Adding  @GET("https://raywenderlich.com") in JokeService.kt

--- app/src/main/java/com/raywenderlich/android/punchline/JokeService.kt
+++ app/src/main/java/com/raywenderlich/android/punchline/JokeService.kt
@@ -31,9 +31,11 @@
 package com.raywenderlich.android.punchline
 
 import io.reactivex.Single
+import retrofit2.http.GET
 
 interface JokeService {
 
+  @GET("https://raywenderlich.com")
   fun getRandomJoke(): Single<Joke>
 
 }
