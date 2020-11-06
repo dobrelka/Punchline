7efe0f2ea254bfd8c318d4feeb9017c4f6cce44a
dobrelka <dobrelyatest@gmail.com>
2020-11-06 16:33:20 +0200

    Adding fun setUp in MainActivityTest.kt

--- app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
@@ -1,9 +1,16 @@
 package com.raywenderlich.android.punchline
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.runner.RunWith
 import org.koin.test.KoinTest
+import org.koin.test.mock.declareMock
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest: KoinTest {
+
+  @Before
+  fun setUp() {
+    declareMock<Repository>()
+  }
 }
