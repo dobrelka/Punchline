393a897a87ed0a2189424f40c22c5e0ea36d28eb
dobrelka <dobrelyatest@gmail.com>
2020-11-06 16:12:32 +0200

    Creating MainActivityTest.kt

---
+++ app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
@@ -0,0 +1,9 @@
+package com.raywenderlich.android.punchline
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+import org.koin.test.KoinTest
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest: KoinTest {
+}
