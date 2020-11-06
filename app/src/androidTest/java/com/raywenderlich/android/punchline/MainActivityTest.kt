da23c5403af7bc9a0e34fcc26ae38aa1870ace94
dobrelka <dobrelyatest@gmail.com>
2020-11-06 16:39:18 +0200

    Adding val mockRepository and val faker in MainActivityTest.kt

--- app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
@@ -1,13 +1,18 @@
 package com.raywenderlich.android.punchline
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.javafaker.Faker
 import org.junit.Before
 import org.junit.runner.RunWith
 import org.koin.test.KoinTest
+import org.koin.test.inject
 import org.koin.test.mock.declareMock
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest: KoinTest {
+
+  private val mockRepository: Repository by inject()
+  private val faker = Faker()
 
   @Before
   fun setUp() {
