a8dcf07d68c253a702138839b3c0fc51d7480cc1
dobrelka <dobrelyatest@gmail.com>
2020-11-07 15:24:42 +0200

    Changed activity_main.xml and fix MainActivityTest.kt

--- app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
@@ -3,6 +3,7 @@ package com.raywenderlich.android.punchline
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.javafaker.Faker
